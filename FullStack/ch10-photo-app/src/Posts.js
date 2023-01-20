1f4833dd1580610aeb371f8da1296832487bb073
chomskim <csknova@gmail.com>
2023-01-20 15:04:43 +0900

    .

--- FullStack/ch10-photo-app/src/Posts.js
+++ FullStack/ch10-photo-app/src/Posts.js
@@ -1,56 +1,57 @@
-import React, { useReducer, useEffect } from 'react';
-import { listPosts } from './graphql/queries';
-import { onCreatePost } from './graphql/subscriptions';
-import { API, graphqlOperation, Storage } from 'aws-amplify';
+import React, { useReducer, useEffect } from 'react'
+import { listPosts } from './graphql/queries'
+import { onCreatePost } from './graphql/subscriptions'
+import { API, graphqlOperation, Storage } from 'aws-amplify'
 
 function reducer(state, action) {
   switch (action.type) {
     case 'SET_POSTS':
-      return action.posts;
+      return action.posts
     case 'ADD_POST':
-      return [action.post, ...state];
+      return [action.post, ...state]
     default:
-      return state;
+      return state
   }
 }
 
 async function getSignedPosts(posts) {
   const signedPosts = await Promise.all(
     posts.map(async (item) => {
-      const signedUrl = await Storage.get(item.imageKey);
-      item.imageUrl = signedUrl;
-      return item;
+      const signedUrl = await Storage.get(item.imageKey)
+      item.imageUrl = signedUrl
+      return item
     })
-  );
-  return signedPosts;
+  )
+  return signedPosts
 }
 
 function Posts() {
-  const [posts, dispatch] = useReducer(reducer, []);
+  const [posts, dispatch] = useReducer(reducer, [])
 
   useEffect(() => {
-    fetchPosts();
+    fetchPosts()
 
     const subscription = API.graphql(graphqlOperation(onCreatePost)).subscribe({
       next: async (post) => {
-        const newPost = post.value.data.onCreatePost;
-        const signedUrl = await Storage.get(newPost.imageKey);
-        newPost.imageUrl = signedUrl;
-        dispatch({ type: 'ADD_POST', post: newPost });
+        const newPost = post.value.data.onCreatePost
+        const signedUrl = await Storage.get(newPost.imageKey)
+        newPost.imageUrl = signedUrl
+        dispatch({ type: 'ADD_POST', post: newPost })
       },
-    });
-    return () => subscription.unsubscribe();
-  }, []);
+    })
+    return () => subscription.unsubscribe()
+  }, [])
 
   async function fetchPosts() {
-    const postData = await API.graphql(graphqlOperation(listPosts));
+    const postData = await API.graphql(graphqlOperation(listPosts))
     const {
       data: {
         listPosts: { items },
       },
-    } = postData;
-    const signedPosts = await getSignedPosts(items);
-    dispatch({ type: 'SET_POSTS', posts: signedPosts });
+    } = postData
+    const signedPosts = await getSignedPosts(items)
+    console.log('signedPosts=', signedPosts)
+    dispatch({ type: 'SET_POSTS', posts: signedPosts })
   }
 
   return (
@@ -58,20 +59,20 @@ function Posts() {
       <h2 style={heading}>Posts</h2>
       {posts.map((post) => (
         <div key={post.id} style={postContainer}>
-          <img style={postImage} src={post.imageUrl} />
+          <img style={postImage} alt={post.title} src={post.imageUrl} />
           <h3 style={postTitle}>{post.title}</h3>
         </div>
       ))}
     </div>
-  );
+  )
 }
 
 const postContainer = {
   padding: '20px 0px 0px',
   borderBottom: '1px solid #ddd',
-};
-const heading = { margin: '20px 0px' };
-const postImage = { width: 400 };
-const postTitle = { marginTop: 4 };
+}
+const heading = { margin: '20px 0px' }
+const postImage = { width: 400 }
+const postTitle = { marginTop: 4 }
 
-export default Posts;
+export default Posts
